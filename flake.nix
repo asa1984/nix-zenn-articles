abc036f0225aadced99e7b8e70d444391a0c4b7a
asa1984 <satoasa9913@gmail.com>
2024-08-22 19:38:13 +0900

    flake.nixを更新

--- flake.nix
+++ flake.nix
@@ -4,43 +4,56 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        nodeEnv =
-          import "${nixpkgs}/pkgs/development/node-packages/node-env.nix" {
-            inherit pkgs;
-            inherit (pkgs)
-              stdenv lib python2 runCommand writeTextFile writeShellScript;
-            nodejs = pkgs.nodejs;
-            libtool = null;
-          };
 
+        nodeEnv = import "${nixpkgs}/pkgs/development/node-packages/node-env.nix" {
+          inherit pkgs;
+          inherit (pkgs)
+            stdenv
+            lib
+            python2
+            runCommand
+            writeTextFile
+            writeShellScript
+            ;
+          nodejs = pkgs.nodejs;
+          libtool = null;
+        };
         zenn-cli = nodeEnv.buildNodePackage rec {
           name = "zenn";
           packageName = "zenn-cli";
-          version = "0.1.153";
+          version = "0.1.154";
           src = builtins.fetchurl {
-            url =
-              "https://registry.npmjs.com/zenn-cli/-/zenn-cli-${version}.tgz";
-            sha256 =
-              "sha256:0jnhxmbgzb2iz4wzgsh8v6p7n4j28lvpn4g0kl92iv84r6sqcz7r";
+            url = "https://registry.npmjs.com/zenn-cli/-/zenn-cli-${version}.tgz";
+            sha256 = "sha256:0q3h1jxlihb2dmlqjrx8wr36mhcd2ppm1cmd6lp2i93hjjrsj0gc";
           };
           production = true;
           bypassCache = true;
           reconstructLock = true;
         };
-        format-script = pkgs.writeScriptBin "format" ''
-          ${pkgs.prettier}/bin/prettier --write .
+
+        formatters = with pkgs; [
+          prettier
+          nixfmt-rfc-style
+          treefmt
+        ];
+        format = pkgs.writeScriptBin "format" ''
+          PATH=$PATH:${pkgs.lib.makeBinPath formatters}
+          ${pkgs.treefmt}/bin/treefmt --config-file ${./treefmt.toml}
         '';
-      in {
+      in
+      {
         packages = {
           default = zenn-cli;
-          format = format-script;
           zenn-cli = zenn-cli;
         };
         devShells.default = pkgs.mkShell { packages = with pkgs; [ typos ]; };
-        formatter = pkgs.nixfmt;
-      });
+        formatter = format;
+      }
+    );
 }
